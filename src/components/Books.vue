032cb6a73fb1074877d49344d93c0cf0f60053f9
fahmiegerton <talk.windows.inblog@live.com>
2022-02-04 18:47:55 +0800

    update how it use

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 .bg-blue-300.flex.flex-col.items-center.justify-center.h-screen.overflow-hidden
 	flipbook.flipbook(
-		:pages="imagesprev",
-		:pages-hi-res="images",
-		:flip-duration="2000",
+		:pages="images || []",
+		:pages-hi-res="gambarZoom",
+		:flip-duration="json.flipDuration || 2000",
 		ref="flipbook"
 	)
 </template>
@@ -17,13 +17,26 @@ const images = [null, ...Object.values(img).map((m) => m.default)];
 const imgprev = import.meta.globEager("../assets/menusprev/*.jpeg");
 const imagesprev = [null, ...Object.values(imgprev).map((m) => m.default)];
 
+const gambar = import.meta.globEager("../assets/gambar.json");
+const json = Object.values(gambar).map((m) => m.default);
+
 export default {
 	components: { Flipbook },
 	data() {
 		return {
 			images,
 			imagesprev,
+			json,
 		};
+	},
+	computed: {
+		gambarZoom() {
+			if (!json?.useSingleSource) {
+				return undefined;
+			} else if (images && images.length == imagesprev.length) {
+				return images;
+			}
+		},
 	},
 };
 </script>
