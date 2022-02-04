b5ebbc2a4636ffbc5b312d1a63c95f7ed33d21a8
fahmiegerton <talk.windows.inblog@live.com>
2022-02-04 12:09:25 +0000

    Deploying to gh-pages from @ fahmiegerton/flipbook-astara@c77765585a5670943131048bc5cf2d17fa330611 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/flipbook-astara/sw.js', { scope: '/flipbook-astara/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
